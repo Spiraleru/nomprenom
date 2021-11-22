8638f062b80850c795dc38c8a167637eac728399
Leo Alves <leoalves@MacBook-Pro-de-Leo-2.local>
2021-11-22 12:01:46 +0100

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,37 @@
 <template>
- <ul id="example-1">
-<li v-for="item in items" :key="item.message">
+<div class="hello"> </div>
+<ul id="example-1">
+<li v-show='toggle' v-for="item in items" :key="item.message">
 {{ item.message }}
 </li>
 </ul>
+
+<div id="example-2">
+<button @click='toggle = !toggle'>Afficher / Masquer</button>
+</div>
+
 </template>
 
-
-
-
 <script>
-export default 
-{name: 'HelloWorld',
-props: {msg: String},
-data () {return {items: [{ message: 'zaza' },{ message: 'tata' },
-{ message: 'toto' },{ message: 'rututu' },{ message: 'Rpopo' }]}}}
+export default {
+name: 'HelloWorld',
+props: {
+msg: String
+},
+data () {
 
-
+return {
+toggle: true,
+items: [
+{ message: 'michmich' },
+{ message: 'balou' },
+{ message: 'rené' },
+{ message: 'zincou' },
+{ message: 'alex' }
+],
+}
+},
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
